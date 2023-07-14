9ba45c57041990d0f3be0272c012812e8a35ba3e
KAMAKSHI <kkamakshiojha@gmail.com>
2023-07-14 20:44:22 +0530

    responsive home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,6 @@ export default function Navbar() {
     <>
      <div class="nav">
         <div class="nav_bar_fixed">
-            
             {/* <div class="nav_left"> */}
                 <ul class="ul a_1">
                     <li><Link class="a_1" to="/">Home</Link></li>
@@ -18,11 +17,24 @@ export default function Navbar() {
                     {/* <li><Link to="">Audio Book</Link></li> */}
                     <li><Link  class="a_1"to="/">About Me</Link></li>
                 </ul>
-
             {/* </div> */}
-            
         </div>
+        <div className='mobile_bar'>
+        <div className='bar1'></div>
+        <div className='bar2'></div>
+        <div className='bar3'></div>
+        <ul class="ul a_1">
+                    <li><Link class="a_1" to="/">Home</Link></li>
+                    <li><Link class="a_1" to="/">Projects</Link></li>
+                    {/* <li><Link to="">All Books</Link></li>  */}
+                    <li><Link class="a_1" to="/">Skills</Link></li>
+                    <li><Link  class="a_1"to="/">Resume</Link></li>
+                    {/* <li><Link to="">Audio Book</Link></li> */}
+                    <li><Link  class="a_1"to="/">About Me</Link></li>
+                </ul>
     </div>
+    </div>
+    
     </>
   )
 }
